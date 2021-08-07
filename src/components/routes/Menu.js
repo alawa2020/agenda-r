4649d8720d0fd33fa3e3ada896c2b5c757f51730
alawa <huder13168@gmail.com>
2021-08-07 00:44:09 -0500

    🍲build menu

--- src/components/routes/Menu.js
+++ src/components/routes/Menu.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
     return (
-        <div>
-            <h1>Hello from menu!</h1>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+
+                    <ul className="navbar-nav">
+                        <Link
+                                className="navbar-brand"
+                                to="/"
+                                aria-current="page"
+                            > Agenda </Link>
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link "
+                                to="/"
+                                aria-current="page"
+                            > Home </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link "
+                                to="/agenda"
+                                aria-current="page"
+                            > Contactos </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link "
+                                to="/login"
+                                aria-current="page"
+                            > Login </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </>
     )
 }
 
-export default Menu
+export default Menu;
