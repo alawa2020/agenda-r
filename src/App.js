c25719ad1b7b0cd6ded2fd73f5845dff989741be
alawa <huder13168@gmail.com>
2021-08-07 00:25:14 -0500

    ↘️ruteo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from 'react';
+import RouteComponent from './components/routes/RouteComponent';
 
 function App() {
   return (
     <div>
-      <h1> hello from app</h1>
+      <RouteComponent />
     </div>
   );
 }
--- src/components/routes/RouteComponent.js
+++ src/components/routes/RouteComponent.js
@@ -5,12 +5,20 @@ import {
     Switch
 } from 'react-router-dom';
 
+import Home from '../Home';
+import Agenda from '../Agenda';
+import Login from '../Login';
+import Menu from './Menu';
+
 const RouteComponent = () => {
     return (
         <>
         <Router>
+            <Menu />
             <Switch>
-                
+                <Route exact path="/" component={Home}/>
+                <Route path="/agenda" component={Agenda} />
+                <Route pathF="/login" component={Login} />
             </Switch>
         </Router>
             
