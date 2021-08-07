a5dc5e53fc5e063315c2029bc450ac42bbc6b846
alawa <huder13168@gmail.com>
2021-08-07 00:17:57 -0500

    🦍creacion componentes home login agenda router

---
+++ src/components/Agenda.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Agenda = () => {
+    return (
+        <div>
+            <h1>Hello from agenda component!</h1>
+        </div>
+    )
+}
+
+export default Agenda
---
+++ src/components/routes/RouteComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+const RouteComponent = () => {
+    return (
+        <>
+        <Router>
+            <Switch>
+                
+            </Switch>
+        </Router>
+            
+
+        </>
+    )
+}
+
+export default RouteComponent
